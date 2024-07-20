93b8772dffbb6a52b75ceefdd4e0890fd05f6e3a
Waheed Khan <waheed066khan@gmail.com>
2024-07-20 15:42:32 +0530

    update profile

---
+++ email.js
@@ -0,0 +1,23 @@
+
+// function emailSend(){
+        
+//     Email.send({
+// Host : "smtp.elasticemail.com",
+// Username : "waheed066khan@gmail.com",
+// Password : "F41A2BFFBC390E6F7DFD449A389D45341028",
+// To : 'codewithwaheedtest@gmail.com',
+// From : "waheed066khan.com",
+// Subject : "This is the subject",
+// Body : "And this is the body"
+// }).then(
+// message => {
+//     if (message=='ok'){
+//     swal("Submitted Successfully", "You clicked the button!", "success");
+// }
+
+//     else{
+//         swal("Error", "You clicked the button!", "error");
+//     }
+// }
+// );
+// }
